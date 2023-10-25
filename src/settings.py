f7f0358566056ba13e687add5629d34e3a719011
erastov-alex <erastov.alex@gmail.com>
2023-10-25 15:18:50 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Gesture v1.0"
 PROJECT_NAME_FULL: str = "Gesture v1.0"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
